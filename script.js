5c3fde54b82432a95b656fa1a6362021963437ec
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-02-08 18:56:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,32 +13,34 @@ while (numberArray.includes(randomNumber)) {
 // Get the current value of the item from local storage
 var currentValue = localStorage.getItem("lastViewedItem");
 localStorage.setItem("lastViewedItem", currentValue + "," + randomFiltered.toString());
-const input = database[randomFiltered];
-function getResults() {
-  let updatedText = input;
-  const parenthesisTextboxes = document.getElementsByClassName(
-    "parenthesis-textbox"
-  );
-  for (let i = 0; i < parenthesisTextboxes.length; i++) {
-    const textboxValue = parenthesisTextboxes[i].value;
-    updatedText = updatedText.replace(
-      `(${matches[i].slice(1, -1)})`, textboxValue
+setTimeout(function() {
+  const input = database[randomFiltered];
+  function getResults() {
+    let updatedText = input;
+    const parenthesisTextboxes = document.getElementsByClassName(
+      "parenthesis-textbox"
     );
-  };
-  const endtextbox = document.createElement("textarea");
-  endtextbox.type = "submit";
-  endtextbox.classList.add("end-textbox");
-  endtextbox.value = updatedText;
-  document.querySelector("#generated").appendChild(endtextbox);
-  const matches = input.match(/\(([^)]+)\)/g);
-  if (matches) {
-    for (let i = 0; i < matches.length; i++) {
-      const strippedMatch = matches[i].slice(1, -1);
-      const textbox = document.createElement("textarea");
-      textbox.type = "text";
-      textbox.classList.add("parenthesis-textbox");
-      textbox.placeholder = strippedMatch;
-      document.querySelector("#output").appendChild(textbox);
+    for (let i = 0; i < parenthesisTextboxes.length; i++) {
+      const textboxValue = parenthesisTextboxes[i].value;
+      updatedText = updatedText.replace(
+        `(${matches[i].slice(1, -1)})`, textboxValue
+      );
+    };
+    const endtextbox = document.createElement("textarea");
+    endtextbox.type = "submit";
+    endtextbox.classList.add("end-textbox");
+    endtextbox.value = updatedText;
+    document.querySelector("#generated").appendChild(endtextbox);
+    const matches = input.match(/\(([^)]+)\)/g);
+    if (matches) {
+      for (let i = 0; i < matches.length; i++) {
+        const strippedMatch = matches[i].slice(1, -1);
+        const textbox = document.createElement("textarea");
+        textbox.type = "text";
+        textbox.classList.add("parenthesis-textbox");
+        textbox.placeholder = strippedMatch;
+        document.querySelector("#output").appendChild(textbox);
+      }
     }
   }
-}
+}, 500);
