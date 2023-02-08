f509ff73bd5e46ea04f54c50f2efd545658a40a6
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-02-07 21:06:49 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 let database = [];
-database.push("idk");
-database.push("idk1");
-database.push("idk2");
-database.push("idk3");
+database.push("It was a dark and (adjective) night. The (noun) was (verb) by the candle light. Slowly a (noun) came (adverb) out of the (noun). (adverb) it (verb) towards the (noun). Then the candle went (adjective) on the (noun).");
+database.push("It was a dark and (adjective) night. It was rainy and (adjective). There was a white (noun). In that white (noun), there was a cannibal. In that cannibal, there was a (noun). The (noun) got out a chainsaw and cut open the man’s (part of body). He laughed (adjective), for he had won the bet if he cut open (part of body) from inside  the man.");
+database.push("It was a dark and (adjective) night. I was (verb) through the snow. The snow was (adjective) and soft. My brother was (verb) stew. My (Noun) was (adjective) in the house and (adjective) with my pet Rhino. My Rhinos name is (name).");
+database.push("I (verb past tense) this morning at (time), and I did 3 things: (verb past tense), (verb past tense), and(verb past tense) the dogs. then(pronoun) showered, and got (adjective) in (adjective) clothes. After that (pronoun) got in the (vehicle) and (past tense verb) to (noun). When (pronoun) got there (pronoun) talked with (plural noun). Soon (noun) started. When (pronoun) got to (place) they started watching CNN (number). Soon (pronoun) got (past tense verb) out of the (place).");
