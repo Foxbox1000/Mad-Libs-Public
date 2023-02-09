616b48cc0a3fbd02d65d554671e6429e6404ffb4
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-02-08 19:13:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,21 +22,21 @@ for (let i = 0; i < parenthesisTextboxes.length; i++) {
     `(${matches[i].slice(1, -1)})`, textboxValue
   );
 };
+const matches = input.match(/\(([^)]+)\)/g);
+if (matches) {
+  for (let i = 0; i < matches.length; i++) {
+    const strippedMatch = matches[i].slice(1, -1);
+    const textbox = document.createElement("textarea");
+    textbox.type = "text";
+    textbox.classList.add("parenthesis-textbox");
+    textbox.placeholder = strippedMatch;
+    document.querySelector("#output").appendChild(textbox);
+  }
+};
 function getResults() {
   const endtextbox = document.createElement("textarea");
   endtextbox.type = "submit";
   endtextbox.classList.add("end-textbox");
   endtextbox.value = updatedText;
   document.querySelector("#generated").appendChild(endtextbox);
-  const matches = input.match(/\(([^)]+)\)/g);
-  if (matches) {
-    for (let i = 0; i < matches.length; i++) {
-      const strippedMatch = matches[i].slice(1, -1);
-      const textbox = document.createElement("textarea");
-      textbox.type = "text";
-      textbox.classList.add("parenthesis-textbox");
-      textbox.placeholder = strippedMatch;
-      document.querySelector("#output").appendChild(textbox);
-    }
-  }
 }
