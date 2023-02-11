fb7be612d7161b3dee634d50c92d152ebbc7769e
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-02-11 13:50:04 -0800

    Update database.js

--- database.js
+++ database.js
@@ -4,4 +4,14 @@ database.push("It was a dark and (adjective) night. The (noun) was (verb) by the
 database.push("It was a dark and (adjective) night. It was rainy and (adjective). There was a white (noun). In that white (noun), there was a cannibal. In that cannibal, there was a (noun). The (noun) got out a chainsaw and cut open the man’s (part of body). He laughed (adjective), for he had won the bet if he cut open (part of body) from inside  the man.");
 database.push("It was a dark and (adjective) night. I was (verb) through the snow. The snow was (adjective) and soft. My brother was (verb) stew. My (Noun) was (adjective) in the house and (adjective) with my pet Rhino. My Rhinos name is (name).");
 database.push("I (verb past tense) this morning at (time), and I did 3 things: (verb past tense), (verb past tense), and(verb past tense) the dogs. then(pronoun) showered, and got (adjective) in (adjective) clothes. After that (pronoun) got in the (vehicle) and (past tense verb) to (noun). When (pronoun) got there (pronoun) talked with (plural noun). Soon (noun) started. When (pronoun) got to (place) they started watching CNN (number). Soon (pronoun) got (past tense verb) out of the (place).");
-database.push("In this pamphlet, I will show you how to get rich (adverb).");
+database.push("In this pamphlet, I will show you how to get rich (adverb).\
+
+Use your (noun) to make more (noun)\
+Invest in the (adjective) market\
+Take out (plural noun) from your local bank or financial institution\
+(verb) your outside spending, focus on business\
+Put your first (noun) made into a frame\
+Keep your profits (adjective) in the bank or your safe\
+Even though family is (adjective), money is more important than anything\
+Find a credit card with good (nouns)\
+Your are rich, congratulations!");
