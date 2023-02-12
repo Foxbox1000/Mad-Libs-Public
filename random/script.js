97746fc7e5c60c7b98a6952954e8d12458181215
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-02-11 17:34:51 -0800

    Update script.js

--- random/script.js
+++ random/script.js
@@ -79,7 +79,7 @@ function getResults() {
     );
   };
   const authortextbox = document.createElement("h1")
-  authortextbox.value = "Author: " + name_database[randomNumber];
+  authortextbox.innerHTML = "Author: " + name_database[randomNumber];
   document.querySelector("#generated").appendChild(authortextbox);
   const endtextbox = document.createElement("textarea");
   endtextbox.type = "text";
