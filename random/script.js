eb1f620f71ae8111bf98d00c8c56f10663432857
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-02-12 19:38:24 -0500

    Update script.js

--- random/script.js
+++ random/script.js
@@ -158,7 +158,7 @@ function getResults() {
     let animating = true;
     function step() {
       let y = currentY + (targetY - currentY) * 0.05;
-      if (window.pageYOffset < targetY) {
+      if (window.pageYOffset < targetY - 2) {
         window.scrollTo(0, y);
         currentY = y;
         window.requestAnimationFrame(step);
