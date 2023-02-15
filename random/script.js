5d727e5f907253c0afc26797909f1aa18b080c1b
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-02-15 12:16:14 -0800

    Update script.js

--- random/script.js
+++ random/script.js
@@ -18,6 +18,8 @@ document.onkeypress = function(event) {
     window.open("/media/jasper.jpg");
   } else if (userInput.endsWith("dog")) {
     window.open("/media/dog.mp4")
+  } else if (userInput.endsWith("will")) {
+    window.open("/media/will.jpg")
   } else if (!/rick/.test(userInput.substr(-6))) {
     userInput = userInput.substr(-5);
   }
